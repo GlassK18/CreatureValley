7abcf681e50ecfb8f9414c9b4b1b8b253decceb5
DRavenMcCoy <43079240+DRavenMcCoy@users.noreply.github.com>
2020-04-19 21:16:36 -0500

    Snake Movement
    The Snake Minigame is in progress. The snake can now move and the sprite turns to match the direction it is heading. The snake also can not turn back on itself.

---
+++ Creature Valley/Assets/Scripts/SnakeAssets.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeAssets : MonoBehaviour
+{
+
+    public static SnakeAssets instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+
+    public Sprite snakeHeadSprite;
+}
---
+++ Creature Valley/Assets/Scripts/SnakeManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+public class SnakeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject snakeHeadGameObject = new GameObject();
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = SnakeAssets.instance.snakeHeadSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
